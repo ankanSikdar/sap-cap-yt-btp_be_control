14be980b75085fba5d39e0a6490166434679cdcb
Ankan Sikdar <ankansikdar@gmail.com>
2026-06-30 00:03:49 +1000

    Invoked Database Procedure

--- srv/ActionHandler.js
+++ srv/ActionHandler.js
@@ -1,3 +1,15 @@
-module.exports.MarkAsObsolete = (req) => {
-    
+const cds = require('@sap/cds');
+
+module.exports.MarkAsObsolete = async (req) => {
+    let reasonComment = req.data.ReasonComment;
+    let assesmentID = req.params[0].ID;
+
+    const tx = cds.transaction(req);
+
+    // Define the query to call the procedure. 
+    const sQuery = 'CALL ST_MARK_AS_OBSOLETE(IN_ASID => ?, IN_REASONCOMMENT => ?)';
+
+    // Execute the procedure
+    const procedure_call = await tx.run(sQuery, [assesmentID, reasonComment]);
+
 }
